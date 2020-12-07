66a7e9ff58eaab778f1f6a904b16a6441ae120b2
Michael Cardona <mcardona9015@gmail.com>
2020-12-07 04:37:40 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def find_item_by_name_in_collection(name, collection)
   collection.each do |item|
-    if item.keys.include? name
+    if item[:item] == name
       item
     end
   end
