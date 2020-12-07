56d6df45ef393e4e0c70faa1206c595f2150d224
Michael Cardona <mcardona9015@gmail.com>
2020-12-07 04:38:43 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,7 @@ require 'pry'
 def find_item_by_name_in_collection(name, collection)
   collection.each do |item|
     if item[:item] == name
-      item
+      return item
     end
   end
   # Implement me first!
